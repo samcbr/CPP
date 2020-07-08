c70a024d2c2b31774eb104cea722e70c4753a8ed
Shivam Gulati <shivamgulati2203@gmail.com>
2020-07-08 23:03:27 +0530

    Rotate image by 90 added

--- Arrays/Rotate Image By 90/main.cpp
+++ Arrays/Rotate Image By 90/main.cpp
@@ -1,21 +1,62 @@
 #include <iostream>
 
 using namespace std;
-void rotateImage(int a[][3],int m,int n)
+void rotateImage(int a[][1000],int n)
 {
-    for(int i=n-1;i>=0;i--)
+    int i=0;
+    int j=n-1;
+    int col;
+    int temp;
+    int row;
+    while(j>=0)
     {
-        for(int j=0;j<m;j++)
+        col=j-1;
+        row=i+1;
+        while(row<=n-1)
         {
-            cout<<a[j][i];
+            temp=a[row][j];
+            a[row][j]=a[i][col];
+            a[i][col]=temp;
+            row++;
+            col--;
+        }
+        j--;
+        i++;
+    }
+    int k=n-1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(k>=j)
+            {
+                swap(a[i][j],a[i][k]);
+            }
+            k--;
+        }
+        k=n-1;
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<a[i][j]<<" ";
         }
         cout<<endl;
     }
 }
 int main()
 {
-    int a[][3]={ {1,2,3},{4,5,6},{7,8,9} };
-    rotateImage(a,3,3);
-    cout << "Hello world!" << endl;
+    int n;
+    cin>>n;
+    int a[n][1000];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    rotateImage(a,n);
     return 0;
 }
